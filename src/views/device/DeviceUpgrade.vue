5f37079d1d271194c86c21d4131b8e3315913e75
Betsy <1147971088@qq.com>
2019-03-05 19:04:11 +0800

    固件升级

--- src/views/device/DeviceUpgrade.vue
+++ src/views/device/DeviceUpgrade.vue
@@ -10,26 +10,17 @@
         <!-- 循环的设备列表 -->
         <el-table :data="firmwareList" style="width: 100%; margin-top: 12px">
             <el-table-column prop="fwID" label="固件ID"></el-table-column>
+            <el-table-column prop="name" label="固件名称"></el-table-column>
             <el-table-column prop="version" label="版本"></el-table-column>
             <el-table-column prop="desc" label="分组"></el-table-column>
             <el-table-column label="操作">
                 <template slot-scope="scope">
                     <el-button
-                        @click="handleUpgrade(scope.row)"
+                        @click.stop="handleUpgrade(scope)"
                         type="text"
                         size="small"
-                        v-if="!(scope.row.fwID == UpgradeFw)"
+                        :disabled="scope.row.status != 0"
                     >升级</el-button>
-                    <div v-else>
-                        <i
-                            class="el-icon-refresh"
-                            style="margin-left: 10px"
-                            @click="updateFwProgress(scope.row)"
-                        ></i>
-                        <div>
-                            <el-progress :percentage="70"></el-progress>
-                        </div>
-                    </div>
                 </template>
             </el-table-column>
         </el-table>
@@ -58,7 +49,8 @@ export default {
                     name: "app",
                     pid: "1z0zbfe0db5",
                     updatedAt: "2019-01-06T11:01:06.935Z",
-                    version: "0.1.2"
+                    version: "0.1.2",
+                    status: 0
                 },
                 {
                     createdAt: "2019-01-06T11:01:06.935Z",
@@ -68,7 +60,8 @@ export default {
                     name: "app",
                     pid: "1z0zbfe0db5",
                     updatedAt: "2019-01-06T11:01:06.935Z",
-                    version: "0.1.2"
+                    version: "0.1.2",
+                    status: 0
                 },
                 {
                     createdAt: "2019-01-06T11:01:06.935Z",
@@ -78,7 +71,8 @@ export default {
                     name: "app",
                     pid: "1z0zbfe0db5",
                     updatedAt: "2019-01-06T11:01:06.935Z",
-                    version: "0.1.2"
+                    version: "0.1.2",
+                    status: 0
                 }
             ]
         };
@@ -94,6 +88,7 @@ export default {
 
     methods: {
         handleUpgrade(fw) {
+            this.firmwareList[fw.$index].status = 1;
             this.UpgradeFw = fw.fwID;
         }
     },
