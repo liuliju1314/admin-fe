4d477dc4d472650ab8ea2b9ac3922502d931f8ae
Betsy <1147971088@qq.com>
2019-04-11 12:03:03 +0800

    更新产品管理

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,73 +1,82 @@
 <template>
     <div class="box-card" shadow="never">
         <el-row :gutter="20">
-            <el-col :span="12">
+            <el-col :span="8">
                 <el-row class="el-content">
                     <!-- 产品信息 -->
-                    <el-col :span="10">
-                        <div class="product-info">
+                    <el-col :span="24">
+                        <div class="product-item">
                             <div class="product-content">
-                                <p class="product-name">
-                                    <span>测试产品</span>
-                                    <span class="product-circle"></span>
-                                    <span style="font-size: 13px;margin:5px 0 0 0">开发中</span>
-                                    <span style="float: right">
-                                        <el-button
-                                            type="primary"
-                                            icon="el-icon-upload"
-                                            size="small"
-                                        >发布</el-button>
+                                <div class="product-name">
+                                    <span style="font-size: 22px; line-height: 32px;">测试产品</span>
+                                    <span>
+                                        <span class="product-circle"></span>
+                                        <span style="font-size: 13px; color: #606266">开发中</span>
                                     </span>
-                                </p>
-                                <p>
-                                    <span class="label">产品ID：</span>
-                                    <span class="text">ck8bufgjc3k</span>
-                                </p>
-                                <p>
-                                    <span class="label">创建时间：</span>
-                                    <span class="text">2019-04-01</span>
-                                </p>
+
+                                    <span style="float: right">
+                                        <el-button type="primary" size="small">发布</el-button>
+                                    </span>
+                                </div>
+                                <div class="product-info">
+                                    <div>
+                                        <span class="label">产品ID：</span>
+                                        <span class="text">
+                                            ck8bufgjc3k
+                                            <el-button
+                                                class="copy-box"
+                                                size="mini"
+                                                @click="copyPid('ck8bufgjc3k')"
+                                                round
+                                            >复制</el-button>
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <span class="label">创建时间：</span>
+                                        <span class="text">2019-04-01</span>
+                                    </div>
+                                </div>
                             </div>
                             <div class="button-box">
-                                <p>
+                                <div>
                                     <el-button
                                         type="primary"
                                         round
                                         class="button-details"
                                         size="small"
                                     >查看产品详情</el-button>
-                                    <el-button type="text" style="float:right;">删除</el-button>
-                                </p>
+                                    <el-button type="text" style="float:right;" size="small">删除</el-button>
+                                </div>
                             </div>
                         </div>
                     </el-col>
                     <!-- 设备信息 -->
-                    <el-col :span="7">
+                    <!-- <el-col :span="7">
                         <div class="device-info">
                             <div class="device-content">
-                                <p>
+                                <div>
                                     <span class="label">当前设备总数：</span>
                                     <span class="device-num">20</span>
-                                </p>
-                                <p>
+                                </div>
+                                <div>
                                     <span class="label">当前在线设备：</span>
                                     <span class="device-num">20</span>
-                                </p>
-                                <p>
+                                </div>
+                                <div>
                                     <span class="label">当前离线设备：</span>
                                     <span class="device-num">0</span>
-                                </p>
+                                </div>
                             </div>
-                            <p>
+                            <div>
                                 <el-button
                                     type="primary"
                                     round
                                     class="device-btn"
                                     size="small"
                                 >查看设备详情</el-button>
-                            </p>
+                            </div>
                         </div>
-                    </el-col>
+                    </el-col>-->
                 </el-row>
             </el-col>
         </el-row>
@@ -75,7 +84,9 @@
 </template>
 
 <script>
+import copy from "@/views/mixins/copy";
 export default {
+    mixins: [copy],
     name: "Home",
     data() {
         return {};
@@ -92,11 +103,28 @@ export default {
     methods: {}
 };
 </script>
+<style lang="less">
+.product-item {
+    .el-button--mini,
+    .el-button--mini.is-round {
+        padding: 4px 12px;
+    };
+    .button-box {
+        .el-button+.el-button {
+            margin: 0 15px;
+        }        
+    }
+
+}
+</style>
+
 <style lang='less' scoped>
 .el-content {
-    margin: 30px;
+    margin: 0px;
     background-color: #fff;
     .label {
+        display: inline-block;
+        width: 70px;
         color: #909399;
     }
     .text {
@@ -110,42 +138,47 @@ p > span {
     margin-left: 8px;
 }
 
-.product-info {
-    border-right: 1px dashed #ebeef5;
+.product-item {
+    // border-right: 1px dashed #ebeef5;
     padding: 20px;
     .product-content {
-        min-height: 120px;
+        height: 120px;
         .product-name {
-            font-size: 20px;
-            overflow: hidden;
-            line-height: 40px;
-            height: 40px;
+            margin-bottom: 12px;
+            height: 32px;
+            line-height: 32ppx;
+        }
+        .product-info {
+            font-size: 13px;
+            & > div {
+                margin-bottom: 10px;
+            }
+            .copy-box {
+                margin-left: 4px;
+            }
         }
         .product-circle {
             width: 6px;
             height: 6px;
             display: inline-block;
+            vertical-align: middle;
             margin: 0 4px 0 10px;
             border-radius: 50%;
-            vertical-align: middle;
             background-color: #1890ff;
         }
     }
-    .button-box {
-        p {
-            overflow: hidden;
-        }
-        .button-details {
-            color: #999;
-            background-color: #fff;
-            border: 1px solid rgb(24, 144, 255);
-        }
-        .button-details:hover {
-            color: #fff;
-            background-color: rgb(24, 144, 255);
-            border: 1px solid rgb(24, 144, 255);
-        }
-    }
+    // .button-box {
+    //     .button-details {
+    //         color: #999;
+    //         background-color: #fff;
+    //         border: 1px solid rgb(24, 144, 255);
+    //     }
+    //     .button-details:hover {
+    //         color: #fff;
+    //         background-color: rgb(24, 144, 255);
+    //         border: 1px solid rgb(24, 144, 255);
+    //     }
+    // }
 }
 .device-info {
     padding: 20px;
--- src/views/product/info/BaseInfo.vue
+++ src/views/product/info/BaseInfo.vue
@@ -49,7 +49,9 @@
 <script>
 import ProductCreate from "@/views/product/ProductCreate";
 import { mapGetters } from "vuex";
+import copy from '@/views/mixins/copy';
 export default {
+    mixins: [copy],
     name: "BaseInfo",
     data() {
         return {
@@ -83,26 +85,7 @@ export default {
                 return "静默升级";
             }
         },
-        copyPid(pid) {
-            let aux = document.createElement("input"); // 创建元素用于复制
-            // 获取复制内容
-            let content = pid;
-            // 设置元素内容
-            aux.setAttribute("value", content);
-            // 将元素插入页面进行调用
-            document.body.appendChild(aux);
-            // 复制内容
-            aux.select();
-            //aux.setSelectionRange(0, inputText.value.length);
-            // 将内容复制到剪贴板
-            document.execCommand("copy", true);
-            // 删除创建元素
-            document.body.removeChild(aux);
-            this.$message({
-                type: "success",
-                message: "复制成功"
-            });
-        }
+
     }
 };
 </script>
