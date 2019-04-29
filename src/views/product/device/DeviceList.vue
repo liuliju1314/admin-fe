01ba5e80764b2443ed651b7822a8e39fa87d5122
Betsy <1147971088@qq.com>
2019-04-29 18:15:31 +0800

    更新deviceList

--- src/views/product/device/DeviceList.vue
+++ src/views/product/device/DeviceList.vue
@@ -535,7 +535,6 @@ export default {
                         });
                         this.handleeEquipment();
                         this.handleClose();
-
                     });
                 }
             });
