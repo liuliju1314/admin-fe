217d31f54cad2f12d32a444e86455acad936d932
Betsy <1147971088@qq.com>
2019-06-03 18:17:24 +0800

    更新路由

--- src/views/redirect/Index.vue
+++ src/views/redirect/Index.vue
@@ -3,10 +3,10 @@ export default {
     beforeCreate() {
         const { params, query } = this.$route;
         const { path } = params;
-        if(!path) {
-            this.$router.replace({ path: "/login", query });
+        if (!path) {
+            this.$router.replace({ path: "/home", query });
         } else {
-            this.$router.replace({ path: "/" + path, query });            
+            this.$router.replace({ path: "/" + path, query });
         }
     },
     render: function(h) {
