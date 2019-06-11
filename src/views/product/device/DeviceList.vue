1194c9e1516b6d0ee2cca620b8445b6d538a12cc
Betsy <1147971088@qq.com>
2019-06-11 14:38:56 +0800

    更新产品创建和规则引擎

--- src/views/product/device/DeviceList.vue
+++ src/views/product/device/DeviceList.vue
@@ -98,7 +98,7 @@
                 </template>
             </el-table-column>
             <el-table-column prop="enable" label="禁用状态"></el-table-column>
-            <el-table-column label="操作">
+            <el-table-column label="操作" width="200">
                 <template slot-scope="scope">
                     <el-button type="text" size="small" @click="expandDetail(scope.row)">查看</el-button>
                     <el-button type="text" size="small" @click="handleUpgrade(scope.row)">升级</el-button>
